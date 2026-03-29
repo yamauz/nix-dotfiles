fee72e58b2c4a16aa144039c014f3b162672f29d
yamauz <yamauzy@gmail.com>
2026-03-29 22:21:08 +0900

    node version

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,6 +68,58 @@
         src = pkgs.fishPlugins.pure.src;
       }
     ];
+    functions = {
+      _pure_prompt_node = ''
+        if test -f package.json; or test -f .nvmrc; or test -f .node-version
+          set -l node_version (node --version 2>/dev/null)
+          if test -n "$node_version"
+            set -l icon (printf '\ue718')
+            echo (set_color a6d189)"$icon $node_version"(set_color normal)
+          end
+        end
+      '';
+      _pure_prompt_first_line = ''
+        set --local prompt_ssh (_pure_prompt_ssh)
+        set --local prompt_container (_pure_prompt_container)
+        set --local prompt_k8s (_pure_prompt_k8s)
+        set --local prompt_git (_pure_prompt_git)
+        set --local prompt_command_duration (_pure_prompt_command_duration)
+        set --local prompt_node (_pure_prompt_node)
+        set --local prompt (_pure_print_prompt \
+                                $prompt_ssh \
+                                $prompt_container \
+                                $prompt_k8s \
+                                $prompt_git \
+                                $prompt_node \
+                                $prompt_command_duration
+                            )
+        set --local prompt_width (_pure_string_width $prompt)
+        set --local current_folder (_pure_prompt_current_folder $prompt_width)
+
+        set --local prompt_components
+        if set --query pure_begin_prompt_with_current_directory; and test "$pure_begin_prompt_with_current_directory" = true
+            set prompt_components \
+                $current_folder \
+                $prompt_git \
+                $prompt_ssh \
+                $prompt_container \
+                $prompt_k8s \
+                $prompt_node \
+                $prompt_command_duration
+        else
+            set prompt_components \
+                $prompt_ssh \
+                $prompt_container \
+                $prompt_k8s \
+                $current_folder \
+                $prompt_git \
+                $prompt_node \
+                $prompt_command_duration
+        end
+
+        echo (_pure_print_prompt $prompt_components)
+      '';
+    };
   };
   programs.zoxide = {
     enable = true;

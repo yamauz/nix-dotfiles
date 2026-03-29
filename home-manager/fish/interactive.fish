56ef3b8bad276fd423f2b59c99e35ac87032d10b
yamauz <yamauzy@gmail.com>
2026-03-29 21:03:12 +0900

    catppuccin style

--- home-manager/fish/interactive.fish
+++ home-manager/fish/interactive.fish
@@ -1,1 +1,13 @@
 # Interactive shell configuration
+
+# Catppuccin Frappé theme for pure
+set --universal pure_color_primary babbf1          # directory (lavender)
+set --universal pure_color_success ef9f76           # prompt symbol on success (peach)
+set --universal pure_color_danger e78284            # prompt symbol on error (red)
+set --universal pure_color_mute a5adce              # git branch, hostname, etc. (subtext0)
+set --universal pure_color_warning e5c890           # command duration (yellow)
+set --universal pure_color_info 8caaee              # git unpulled/unpushed (blue)
+set --universal pure_color_light c6d0f5             # root username (text)
+set --universal pure_color_normal c6d0f5            # jobs (text)
+set --universal pure_color_git_branch ca9ee6        # git branch override (mauve)
+set --universal pure_color_git_dirty e5c890         # git dirty (yellow)

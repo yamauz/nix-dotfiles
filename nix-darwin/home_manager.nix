89431c1ae0aa7d8bdde41f6c0cc5c297ea0f2b13
yamauz <yamauzy@gmail.com>
2026-03-31 09:31:42 +0900

    vscode

--- nix-darwin/home_manager.nix
+++ nix-darwin/home_manager.nix
@@ -1,5 +1,6 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
   home-manager.users."yamauz" = ../home-manager/home.nix;
 }
